9cdd95b78ac72a86f81bef5b95dbde426beeec7f
Esmé Taylor Richardson <reese.taylor.richardson@emory.edu>
2024-01-31 01:14:58 -0500

    added fields to enter course and topic separately

--- SGGUI.py
+++ SGGUI.py
@@ -1,4 +1,6 @@
 import wx
+from wx import LIST_AUTOSIZE, LIST_FORMAT_CENTER
+
 import OpenAISetup
 
 
@@ -7,7 +9,7 @@ class SGGUI(wx.Frame):
         super().__init__(parent=None,
                          title="Ngholomennod's Study Guide Generator")
         self.panel = SGGUI_Panel(self)
-        self.SetSize(770, 300)
+        self.SetSize(770, 360)
         self.Show()
 
 
@@ -15,42 +17,57 @@ class SGGUI_Panel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         my_sizer = wx.BoxSizer(wx.VERTICAL)
+        course_label = wx.StaticText(self, label="Course")
+        topic_label = wx.StaticText(self, label="Topic(s)")
         self.text_ctrl = wx.TextCtrl(self)
+        self.text_ctrl2 = wx.TextCtrl(self)
         instructions = wx.StaticText(self, label="Please enter a course name followed by a topic like so -> (ex. "
                                                  "Computer Science, big-o notation) then click [Add Study-Guide]."
                                                  "\nAfter you add your guides, "
                                                  "clicking on the [Generate Study-Guide(s)] button will produce a "
-                                                 "text file for each guide."
-                                                 "\nEnter COURSE, TOPIC or COURSE, "
-                                                 "[TOPIC1, TOPIC2, TOPIC3]:")
+                                                 "text file for each guide.\n")
         submit_btn = wx.Button(self, label='Generate Study-Guide(s)')
         submit_btn.Bind(wx.EVT_BUTTON, self.on_submit)
         add_btn = wx.Button(self, label='Add Study-Guide')
         add_btn.Bind(wx.EVT_BUTTON, self.on_add)
 
-        self.list_ctrl = wx.ListCtrl(self, size=(-1, 100),
+        self.list_ctrl = wx.ListCtrl(self,
                                      style=wx.LC_REPORT | wx.BORDER_SUNKEN)
         self.list_ctrl.row_obj_dict = {}
-        self.list_ctrl.InsertColumn(0, '<Course, Topic(s)>', width=800)
+        self.list_ctrl.InsertColumn(0, 'Course', width=270)
+        self.list_ctrl.InsertColumn(1, 'Topic(s)', width=470)
+
         my_sizer.Add(instructions, 0, wx.ALL | wx.LEFT, 5)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+
+        my_sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        my_sizer2.AddSpacer(5)
+        my_sizer2.Add(course_label, wx.ALL | wx.ALIGN_LEFT, 5)
+        my_sizer2.Add(self.text_ctrl, 0, wx.ALL | wx.ALIGN_LEFT, 5)
+        my_sizer2.AddSpacer(110)
+        my_sizer2.Add(topic_label, wx.ALL | wx.ALIGN_LEFT, 5)
+        my_sizer2.Add(self.text_ctrl2, 0, wx.ALL | wx.ALIGN_LEFT, 5)
+        my_sizer.Add(my_sizer2, 1)
+
         my_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)
 
-        my_sizer.Add(add_btn, 0, wx.ALL | wx.CENTER, 5)
-        my_sizer.Add(submit_btn, 0, wx.ALL | wx.CENTER, 5)
+        my_sizer.Add(add_btn, 0, wx.ALL | wx.LEFT, 5)
+        my_sizer.Add(submit_btn, 0, wx.ALL | wx.LEFT, 5)
         self.SetSizer(my_sizer)
 
     def on_add(self, event):
-        message = self.text_ctrl.GetValue()
-        if not message:
+        course = self.text_ctrl.GetValue()
+        topic = self.text_ctrl2.GetValue()
+        if not course:
             print("You didn't enter anything!")
         else:
             print("Adding...")
-            self.list_ctrl.InsertItem(0, message)
+            self.list_ctrl.InsertItem(0, course)
+            self.list_ctrl.InsertItem(1, topic)
+
 
     def on_submit(self, event):
         index = 0
         while index < self.list_ctrl.GetItemCount():
             print("Communicating with OpenAI...")
-            OpenAISetup.submit(self.list_ctrl.GetItemText(index))
+            OpenAISetup.submit(self.list_ctrl.GetItemText(index, 0) + ', ' + self.list_ctrl.GetItemText(index, 1))
             index += 1
